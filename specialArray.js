49626df118d56cae71e05d7c9677955ad1d5b5c1
quackquavk <pukarkhanal4@gmail.com>
2024-05-27 07:01:44 +0545

    SpecialArray

--- specialArray.js
+++ specialArray.js
@@ -1,11 +1,10 @@
-var specialArray = function (nums) {
-  nums.filter((num) => num !== 0);
-  nums.sort();
-  let specialNum=0;
-  for (let i = 0; i < nums.length; i++) {
-        if(nums[i] >= specialNum){
-            
-        }
-  }
-};
-console.log(specialArray([1, 0, 4, 0]));
+var specialArray = function(nums) {
+    for(i = 0; i <= nums.length; i++){
+        let c = 0;
+     for(let num of nums){
+         num >= i && ++c;
+     }
+     if(c === i) return i;
+    } 
+    return -1;
+ };
